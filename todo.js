be7919099ecd49a582f5df664d2f5f147afd5e43
yasser-aboufaris <azertyasser321@gmail.com>
2024-11-01 19:18:38 +0100

    create the js to show and hide the task form

--- todo.js
+++ todo.js
@@ -1,1 +1,21 @@
-Document
+//////////////////////////////////////////////////////
+document.getElementById("add-button").addEventListener("click",function () {
+    document.getElementById("add-form").style.display="block";
+});
+
+document.getElementById("add-close").addEventListener("click",function () {
+    document.getElementById("add-form").style.display="none";
+});
+////////////////////////////////////////////////////
+
+document.getElementById("create-task").addEventListener("click",function(){
+    let title = document.getElementById("taskName").value;
+    let description = document.getElementById("taskDescription").value;
+    let priority = document.getElementById("priority").value;
+    let taskTime = document.getElementById("taskTime").value;
+    let Statue = document.getElementById("Statue").value;
+    console.log(description)
+    console.log(priority)
+    console.log(taskTime)
+    console.log(Statue)
+})
