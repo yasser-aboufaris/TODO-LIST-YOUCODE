c923a04a565f83dd9b7574f21af9df2008bfcb53
yasser-aboufaris <azertyasser321@gmail.com>
2024-11-01 19:54:36 +0100

    finish the add part

--- todo.js
+++ todo.js
@@ -14,8 +14,24 @@ document.getElementById("create-task").addEventListener("click",function(){
     let priority = document.getElementById("priority").value;
     let taskTime = document.getElementById("taskTime").value;
     let Statue = document.getElementById("Statue").value;
-    console.log(description)
-    console.log(priority)
-    console.log(taskTime)
-    console.log(Statue)
+
+    
+    document.getElementById(Statue).innerHTML += `<div class="max-w-xs mx-auto mb-2 mt-1">
+                                                            <div class="bg-white p-3 rounded shadow">
+                                                                <h2 class="text-lg font-bold">${title}</h2>
+                                                                <p class="text-sm">Description:${description} </p>
+                                                                <p class="text-sm">Time: ${taskTime}</p>
+                                                                <p class="text-sm">Status:
+                                                                <select class="border border-gray-300 rounded">
+                                                                    <option>To Do</option>
+                                                                    <option>Doing</option>
+                                                                    <option>Done</option>
+                                                                </select>
+                                                                </p>
+                                                                <button class="bg-blue-500 text-white py-1 px-3 rounded mt-2 text-sm">
+                                                                Mark as Complete
+                                                                </button>
+                                                            </div>
+                                                    </div>`
+
 })
