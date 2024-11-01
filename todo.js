7684f2c1f1baabd90fb91246cc6c98a02fdde0f5
yasser-aboufaris <azertyasser321@gmail.com>
2024-11-01 23:30:05 +0100

    finished the delete part and starting on the modifier

--- todo.js
+++ todo.js
@@ -7,7 +7,7 @@ document.getElementById("add-close").addEventListener("click",function () {
     document.getElementById("add-form").style.display="none";
 });
 ////////////////////////////////////////////////////
-
+let counter=0;
 document.getElementById("create-task").addEventListener("click",function(){
     let title = document.getElementById("taskName").value;
     let description = document.getElementById("taskDescription").value;
@@ -15,23 +15,32 @@ document.getElementById("create-task").addEventListener("click",function(){
     let taskTime = document.getElementById("taskTime").value;
     let Statue = document.getElementById("Statue").value;
 
+    let taskId = Date.now();
     
-    document.getElementById(Statue).innerHTML += `<div class="max-w-xs mx-auto mb-2 mt-1">
+    document.getElementById(Statue).innerHTML += `<div id="${taskId}" class="max-w-xs mx-auto mb-2 mt-1">
                                                             <div class="bg-white p-3 rounded shadow">
                                                                 <h2 class="text-lg font-bold">${title}</h2>
                                                                 <p class="text-sm">Description:${description} </p>
                                                                 <p class="text-sm">Time: ${taskTime}</p>
-                                                                <p class="text-sm">Status:
-                                                                <select class="border border-gray-300 rounded">
-                                                                    <option>To Do</option>
-                                                                    <option>Doing</option>
-                                                                    <option>Done</option>
-                                                                </select>
-                                                                </p>
-                                                                <button class="bg-blue-500 text-white py-1 px-3 rounded mt-2 text-sm">
-                                                                Mark as Complete
+                                                                <p class="text-sm">priority: ${priority}</p>
+                                                                <p class="text-sm">Status:${Statue} </p>
+                                                                <button class="bg-blue-500 text-white py-1 px-3 rounded mt-2 text-sm change">
+                                                                change
+                                                                </button>
+                                                                <button class="bg-blue-500 text-white py-1 px-3 rounded mt-2 text-sm delete">
+                                                                delete
                                                                 </button>
                                                             </div>
                                                     </div>`
 
+
+    document.getElementById(taskId).querySelector(".delete").addEventListener("click", deleteTask);
 })
+///////////////////////////////////////////////////////////////////:
+document.querySelector("# .class")
+
+
+function deleteTask(event) {
+    event.target.parentElement.parentElement.remove();
+}
+////////////////////////////////////////////////////////////////////////
